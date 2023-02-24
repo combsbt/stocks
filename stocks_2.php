4ff6b8ef8a3496bc04809c94201d1c25c257e153
combsbt <combsbt@vcu.edu>
2023-02-24 10:16:01 -0500

    save

--- stocks_2.php
+++ stocks_2.php
@@ -31,17 +31,18 @@
     <input type="text" id="thisTrade" name="thisTrade" hidden="true" value="">
     <input type="text" id="tradeInfo" name="tradeInfo" hidden="true" value="">
   </form>
+  <div id="progress">
+  </div>
 
 <?php
   if(array_key_exists('submitButton', $_POST)) {
-  echo '<script>localStorage.setItem("tradeDate", null);</script>';
   $username = "root";
   $password = "";
   $hostname = "localhost"; 
   $database="Stocks";
 
   //connection to the mysql database,
-  $dbhandle = mysqli_connect($hostname, $username, $password,$database )
+  $dbhandle = mysqli_connect($hostname, $username, $password, $database)
   or die("Unable to connect to MySQL");
   echo "Connected to MySQL<br>";
 
@@ -50,22 +51,20 @@
   $result = mysqli_query($dbhandle, "SELECT DISTINCT table_name FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME IN ('Date') AND TABLE_SCHEMA='Stocks'" );
 
   $startDate = $_POST['startDate'];
-  echo
-  '
-  <script>
-    localStorage.setItem("startDate", '.json_encode($startDate).');
-  </script>
-  ';
+  echo "Getting data from ".$startDate;
   //fetch tha data from the database 
   $testArray = array();
   $fullList = array();
   $fullSpy = array();
+  $count = 0;
   while ($row = mysqli_fetch_array($result)) {
     // get rid of tickers with bad names for now
     if( $row[0] != "all" && $row[0] != "brk-b" && $row[0] != "key" && $row[0] != "keys"){
     // select rows where all 3 indicator conditions are met for selling
     $newRes = mysqli_query($dbhandle, "SELECT * FROM $row[0] WHERE $row[0].Date >= '$startDate' AND ($row[0].Close > $row[0].bb_up AND $row[0].rsi > 70 AND $row[0].ult > 70) ");
     while($test = mysqli_fetch_array($newRes)){
+      $count = $count + 1;
+      echo "<script>document.getElementById('progress').innerHTML = ".json_encode($count)."</script>";
       $fullList[explode(" ", $test[0])[0]." ".$row[0]] = $test;
       // echo explode(" ", $test[0])[0]." SELL ".$row[0]." at ".$test["Close"].
       // " check ".date('Y-m-d', strtotime($test[0]. ' + 2 days'))."<br>";
@@ -80,6 +79,8 @@
     }
     $newRes2 = mysqli_query($dbhandle, "SELECT * FROM $row[0] WHERE $row[0].Date >= '$startDate' AND ($row[0].ult < 30 AND $row[0].Close < $row[0].bb_low AND $row[0].rsi < 30) ");
     while($test = mysqli_fetch_array($newRes2)){
+      $count = $count + 1;
+      echo "<script>document.getElementById('progress').innerHTML = ".json_encode($count)."</script>";
       $fullList[explode(" ", $test[0])[0]." ".$row[0]] = $test;
       // echo explode(" ", $test[0])[0]." BUY ".$row[0]." at ".$test["Close"].
       // " check ".date('Y-m-d', strtotime($test[0]. ' + 2 days'))."<br>";
@@ -111,17 +112,20 @@
   localStorage.setItem("fullList", JSON.stringify(fullList));
   localStorage.setItem("testArray", JSON.stringify(testArray));
   localStorage.setItem("fullSpy", JSON.stringify(fullSpy));
+  localStorage.setItem("startDate", '.json_encode($startDate).');
+  document.getElementById("start").value = '.json_encode($startDate).';
   </script>
   ';
 
   //close the connection
   mysqli_close($dbhandle);
+  echo '<script>document.getElementById("testButton").hidden = false</script>';
 }
 
 ?>
 <div id="myPlot" style="width:100%;max-width:700px;margin:auto"></div>
 <div>
-  <button onclick = "testFunction(document.getElementById('start').value, 0)" >Test</button>
+  <button onclick = "testFunction(document.getElementById('start').value, 0)" id="testButton">Test</button>
 </div>
 <br/>
 <div id="buttons">
@@ -142,7 +146,6 @@
     }
     itmsByDate[date].push(Object.entries(fullList)[idx][0])
   })
-  console.log(dateList)
   localStorage.setItem("dateList", JSON.stringify(dateList));
   localStorage.setItem("itmsByDate", JSON.stringify(itmsByDate));
 
@@ -163,16 +166,14 @@
       itmsByDate[nextTrade].forEach(trade=>{
         let divTotal = total/itmsByDate[nextTrade].length
         total = total + divTotal*testArray[trade];
-        console.log(trade)
-        console.log(total)
       })
     }
     if(nextTrade){
       totals[nextTrade] = total;
       return testFunction(new Date(nextTrade), totals)
     }
-    console.log(totals)
     plotTotals(totals);
+    document.getElementById("testButton").hidden = true;
   }
 
   function plotTotals(totals){
@@ -189,13 +190,15 @@
       var data = [{
         x:xArray,
         y:yArray,
-        mode:"line"
+        mode:"line",
+        name:"strategy"
       },
       {
 
         x:Object.keys(totals),
         y:spyValues,
-        mode:"line"
+        mode:"line",
+        name:"s&p500"
       }
       ];
       
