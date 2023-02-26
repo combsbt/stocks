f3c9e19a9df6439a4e9f2747462b539253563329
combsbt <combsbt@vcu.edu>
2023-02-26 13:58:04 -0500

    indexedDB

--- stocks_2.php
+++ stocks_2.php
@@ -49,6 +49,8 @@
   <button onclick = "testFunction(document.getElementById('start').value, 0, 10000)" id="testButton" hidden="true">Show Plot</button>
 </div>
 <script>
+    !function(){function e(t,o){return n?void(n.transaction("s").objectStore("s").get(t).onsuccess=function(e){var t=e.target.result&&e.target.result.v||null;o(t)}):void setTimeout(function(){e(t,o)},100)}var t=window.indexedDB||window.mozIndexedDB||window.webkitIndexedDB||window.msIndexedDB;if(!t)return void console.error("indexDB not supported");var n,o={k:"",v:""},r=t.open("d2",1);r.onsuccess=function(e){n=this.result},r.onerror=function(e){console.error("indexedDB request error"),console.log(e)},r.onupgradeneeded=function(e){n=null;var t=e.target.result.createObjectStore("s",{keyPath:"k"});t.transaction.oncomplete=function(e){n=e.target.db}},window.ldb={get:e,set:function(e,t){o.k=e,o.v=t,n.transaction("s","readwrite").objectStore("s").put(o)}}}();
+    
     if(localStorage.getItem("startDate")){
       console.log(localStorage.getItem("startDate"));
       document.getElementById("start").value = localStorage.getItem("startDate");
@@ -309,6 +311,7 @@
   var ult2 = '.json_encode($_POST["ult2"]).';
   document.getElementById("progress").innerHTML = "Total trades analyzed: '.json_encode($count).'"
   try{
+    ldb.set("fullList", JSON.stringify(fullList));
     localStorage.setItem("fullList", JSON.stringify(fullList));  
   } catch (error) {
     console.log(error)
